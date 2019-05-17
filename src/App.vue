c055b0b759bcc86c3aeb374cc2402dc21589961b
Simas <simonas@digitouch.lt>
2019-05-17 08:34:01 +0300

    add vue boilerplate

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
